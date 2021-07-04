70a635d55e9a1d2472c131742afa21e4589ddc3b
mahendra <desaimahi1996@gmail.com>
2021-07-04 10:27:25 +0530

    WH-9696-EmployeeREst cretaed

---
+++ src/main/java/com/mahi/einfo/SpringBootCloudProductServiceproducer1Application.java
@@ -0,0 +1,13 @@
+package com.mahi.einfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCloudProductServiceproducer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCloudProductServiceproducer1Application.class, args);
+	}
+
+}
