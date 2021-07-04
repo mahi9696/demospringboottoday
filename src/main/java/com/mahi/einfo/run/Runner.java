158b31b78904004cdbd0f2c4fe4b4c25e7aa2b67
mahendra <desaimahi1996@gmail.com>
2021-07-04 10:40:33 +0530

    wh-9696-employee modify

---
+++ src/main/java/com/mahi/einfo/run/Runner.java
@@ -0,0 +1,12 @@
+package com.mahi.einfo.run;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class Runner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("demo");
+	}
+
+}
